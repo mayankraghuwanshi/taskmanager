11626b1ef5b83a9a89baa007ff34c2ed79774071
mayank raghuvanshi <mayank1272@hotmail.com>
2020-05-26 08:44:49 +0530

    Updates

---
+++ precache-manifest.bfe646ba49619fa3ba198e41cc299f7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40250021070d17b04b69d6ffc862e3a9",
+    "url": "/taskmanager/index.html"
+  },
+  {
+    "revision": "a82244ca3a5f267bec07",
+    "url": "/taskmanager/static/css/main.094fd9c2.chunk.css"
+  },
+  {
+    "revision": "2f57a3168245dc29d083",
+    "url": "/taskmanager/static/js/2.3dacb813.chunk.js"
+  },
+  {
+    "revision": "c575066c44ef7fd5ad5a1120ac7a415c",
+    "url": "/taskmanager/static/js/2.3dacb813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a82244ca3a5f267bec07",
+    "url": "/taskmanager/static/js/main.cb45ce71.chunk.js"
+  },
+  {
+    "revision": "1dff4b5ec511ebf56813",
+    "url": "/taskmanager/static/js/runtime-main.12aa24dd.js"
+  }
+]);
